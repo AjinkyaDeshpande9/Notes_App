1e581d4091e491628e3a860ab1c7b7b2cf2a0616
Ajinkya <deshpande.ajinkya09@gmail.com>
2022-06-23 19:34:33 +0530

    Entities Created

---
+++ app/src/main/java/com/example/notesapp/Model/Notes.kt
@@ -0,0 +1,15 @@
+package com.example.notesapp.Model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Notes")
+class Notes(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
+    var title: String,
+    var subTitle: String,
+    var notes: String,
+    var date: String,
+    var priority:String
+)
