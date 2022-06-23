c4b5d006eb64872f89f5cf7c0624dd2b1419a521
Ajinkya <deshpande.ajinkya09@gmail.com>
2022-06-23 13:47:27 +0530

    All major components UI Created

--- app/src/main/java/com/example/notesapp/ui/Fragments/CreateNotesFragment.kt
+++ app/src/main/java/com/example/notesapp/ui/Fragments/CreateNotesFragment.kt
@@ -6,14 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.notesapp.R
+import com.example.notesapp.databinding.FragmentCreateNotesBinding
+
 class CreateNotesFragment : Fragment() {
+
+    lateinit var binding: FragmentCreateNotesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_notes, container, false)
+        binding = FragmentCreateNotesBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/notesapp/ui/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/notesapp/ui/Fragments/HomeFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.notesapp.R
+import com.example.notesapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater,container,false)
+        binding.btnAddNotes.setOnClickListener{
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_createNotesFragment)
+        }
+        return binding.root
     }
 
 }
