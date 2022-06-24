1c857a78cfcfc807b2cd78f957c182eddb447f5b
Ajinkya <deshpande.ajinkya09@gmail.com>
2022-06-24 08:19:38 +0530

    Database Created

---
+++ app/src/main/java/com/example/notesapp/Database/NotesDatabase.kt
@@ -0,0 +1,31 @@
+package com.example.notesapp.Database
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.notesapp.Dao.NotesDao
+
+abstract class NotesDatabase:RoomDatabase() {
+    abstract fun myNotesDao():NotesDao
+
+    companion object
+    {
+        @Volatile
+        var INSTANCE: NotesDatabase?=null
+
+        fun getDatabaseInstance(context:Context):NotesDatabase{
+            var tempInstance = INSTANCE
+            if(tempInstance!=null)
+            {
+                return tempInstance
+            }
+            synchronized(this)
+            {
+                val roomDatabaseInstance =
+                    Room.databaseBuilder(context, NotesDatabase::class.java,"Notes").build()
+                INSTANCE= roomDatabaseInstance
+                return roomDatabaseInstance
+            }
+        }
+    }
+}
