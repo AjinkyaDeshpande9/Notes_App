907e849f9e41a924d0202e19deef887f5839f8d1
Ajinkya <deshpande.ajinkya09@gmail.com>
2022-06-24 08:23:58 +0530

    Notes Repository Created

---
+++ app/src/main/java/com/example/notesapp/Repository/NotesRepository.kt
@@ -0,0 +1,24 @@
+package com.example.notesapp.Repository
+
+import androidx.lifecycle.LiveData
+import com.example.notesapp.Dao.NotesDao
+import com.example.notesapp.Model.Notes
+
+class NotesRepository(val dao: NotesDao) {
+
+    fun getAllNotes(): LiveData<List<Notes>>{
+        return dao.getNotes()
+    }
+
+    fun insertNotes(notes: Notes){
+        dao.insertNotes(notes)
+    }
+
+    fun deleteNotes(id: Int){
+        dao.deleteNotes(id)
+    }
+
+    fun updateNotes(notes: Notes){
+        dao.updateNotes(notes)
+    }
+}
