16cb5b3acdf17d34c99ae3fa5b2d3dc1767de5fe
Ajinkya <deshpande.ajinkya09@gmail.com>
2022-06-25 09:15:28 +0530

    Notes made Parcelize

--- app/src/main/java/com/example/notesapp/Model/Notes.kt
+++ app/src/main/java/com/example/notesapp/Model/Notes.kt
@@ -1,15 +1,20 @@
 package com.example.notesapp.Model
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
 
 @Entity(tableName = "Notes")
+@Parcelize
 class Notes(
     @PrimaryKey(autoGenerate = true)
     var id: Int? = null,
-    var title: String,
+    var title: String?,
     var subTitle: String,
     var notes: String,
     var date: String,
     var priority:String
-)
+):Parcelable
